73f1b2f0250ab34cf50ab2a73fd0b23f14496356
PatrykKups <pkups@anaconda.com>
2025-07-03 13:57:10 +0200

    aws-c-sdkutils 0.2.4  (#4)

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,9 +8,9 @@ cmake ${CMAKE_ARGS} -GNinja \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_INSTALL_LIBDIR=lib \
-  -DENABLE_TESTING=ON \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=ON \
+  -DBUILD_TESTING=ON \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
   ..
 cmake --build . --config Release --target install
